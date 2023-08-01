fe537984c109833c0ef38e293e59485a3733fd82
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-08-01 17:43:40 -0400

    function continued

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+
+  const wrapAdjective = (star = "*") => (adjective = "dedicated")  => {
+    return `You are ${star}${adjective}${star}!`;
+  }
+  
